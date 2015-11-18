283f69da4b3739d3a83b873965101f09d464fdb1
Sodom Gomora <sodom.gomora@outlook.de>
2015-11-18 19:38:24 +0100

    Password change added.

--- TrueCrypt Mounter/Password.Designer.cs
+++ TrueCrypt Mounter/Password.Designer.cs
@@ -38,12 +38,15 @@
             this.buttonReset = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonChangePassword = new System.Windows.Forms.Button();
+            this.textBoxOldPassword = new System.Windows.Forms.TextBox();
+            this.labelOldPassword = new System.Windows.Forms.Label();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxPassword_first
             // 
-            this.textBoxPassword_first.Location = new System.Drawing.Point(12, 29);
+            this.textBoxPassword_first.Location = new System.Drawing.Point(12, 58);
             this.textBoxPassword_first.Name = "textBoxPassword_first";
             this.textBoxPassword_first.Size = new System.Drawing.Size(257, 20);
             this.textBoxPassword_first.TabIndex = 0;
@@ -52,7 +55,7 @@
             // 
             // textBoxPassword_second
             // 
-            this.textBoxPassword_second.Location = new System.Drawing.Point(12, 66);
+            this.textBoxPassword_second.Location = new System.Drawing.Point(12, 95);
             this.textBoxPassword_second.Name = "textBoxPassword_second";
             this.textBoxPassword_second.Size = new System.Drawing.Size(257, 20);
             this.textBoxPassword_second.TabIndex = 1;
@@ -62,7 +65,7 @@
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(113, 92);
+            this.buttonOK.Location = new System.Drawing.Point(113, 150);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 23);
             this.buttonOK.TabIndex = 2;
@@ -72,7 +75,7 @@
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(194, 92);
+            this.buttonCancel.Location = new System.Drawing.Point(194, 150);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 3;
@@ -83,7 +86,7 @@
             // labelPassword_first
             // 
             this.labelPassword_first.AutoSize = true;
-            this.labelPassword_first.Location = new System.Drawing.Point(9, 13);
+            this.labelPassword_first.Location = new System.Drawing.Point(9, 44);
             this.labelPassword_first.Name = "labelPassword_first";
             this.labelPassword_first.Size = new System.Drawing.Size(53, 13);
             this.labelPassword_first.TabIndex = 5;
@@ -92,7 +95,7 @@
             // labelPassword_second
             // 
             this.labelPassword_second.AutoSize = true;
-            this.labelPassword_second.Location = new System.Drawing.Point(9, 52);
+            this.labelPassword_second.Location = new System.Drawing.Point(9, 81);
             this.labelPassword_second.Name = "labelPassword_second";
             this.labelPassword_second.Size = new System.Drawing.Size(90, 13);
             this.labelPassword_second.TabIndex = 6;
@@ -100,7 +103,7 @@
             // 
             // buttonReset
             // 
-            this.buttonReset.Location = new System.Drawing.Point(13, 92);
+            this.buttonReset.Location = new System.Drawing.Point(12, 150);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(75, 23);
             this.buttonReset.TabIndex = 4;
@@ -112,7 +115,7 @@
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 137);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 178);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(284, 22);
             this.statusStrip1.SizingGrip = false;
@@ -125,11 +128,40 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
             this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
             // 
+            // buttonChangePassword
+            // 
+            this.buttonChangePassword.Location = new System.Drawing.Point(12, 121);
+            this.buttonChangePassword.Name = "buttonChangePassword";
+            this.buttonChangePassword.Size = new System.Drawing.Size(103, 23);
+            this.buttonChangePassword.TabIndex = 9;
+            this.buttonChangePassword.Text = "Password change";
+            this.buttonChangePassword.UseVisualStyleBackColor = true;
+            this.buttonChangePassword.Click += new System.EventHandler(this.buttonChangePassword_Click);
+            // 
+            // textBoxOldPassword
+            // 
+            this.textBoxOldPassword.Location = new System.Drawing.Point(12, 22);
+            this.textBoxOldPassword.Name = "textBoxOldPassword";
+            this.textBoxOldPassword.Size = new System.Drawing.Size(257, 20);
+            this.textBoxOldPassword.TabIndex = 10;
+            // 
+            // labelOldPassword
+            // 
+            this.labelOldPassword.AutoSize = true;
+            this.labelOldPassword.Location = new System.Drawing.Point(9, 9);
+            this.labelOldPassword.Name = "labelOldPassword";
+            this.labelOldPassword.Size = new System.Drawing.Size(72, 13);
+            this.labelOldPassword.TabIndex = 11;
+            this.labelOldPassword.Text = "Old Password";
+            // 
             // Password
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 159);
+            this.ClientSize = new System.Drawing.Size(284, 200);
+            this.Controls.Add(this.labelOldPassword);
+            this.Controls.Add(this.textBoxOldPassword);
+            this.Controls.Add(this.buttonChangePassword);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.labelPassword_second);
@@ -160,5 +192,8 @@
         private System.Windows.Forms.Button buttonReset;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button buttonChangePassword;
+        private System.Windows.Forms.TextBox textBoxOldPassword;
+        private System.Windows.Forms.Label labelOldPassword;
     }
 }
