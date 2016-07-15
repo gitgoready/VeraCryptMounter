adb110f908b5026007aac88d12889eec9366b352
Sodom Gomora <sodom.gomora@outlook.de>
2016-07-15 11:09:23 +0200

    lolo

--- TrueCrypt Mounter/NewContainer.Designer.cs
+++ TrueCrypt Mounter/NewContainer.Designer.cs
@@ -77,6 +77,7 @@
             // 
             // buttonOpenContainer
             // 
+            this.buttonOpenContainer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.buttonOpenContainer.Location = new System.Drawing.Point(8, 55);
             this.buttonOpenContainer.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonOpenContainer.Name = "buttonOpenContainer";
@@ -93,7 +94,7 @@
             this.groupBoxDescription.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxDescription.Name = "groupBoxDescription";
             this.groupBoxDescription.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxDescription.Size = new System.Drawing.Size(555, 62);
+            this.groupBoxDescription.Size = new System.Drawing.Size(494, 62);
             this.groupBoxDescription.TabIndex = 1;
             this.groupBoxDescription.TabStop = false;
             this.groupBoxDescription.Text = "Beschreibung des Kontainers";
@@ -103,7 +104,7 @@
             this.textBoxDescription.Location = new System.Drawing.Point(8, 23);
             this.textBoxDescription.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.Size = new System.Drawing.Size(537, 22);
+            this.textBoxDescription.Size = new System.Drawing.Size(465, 20);
             this.textBoxDescription.TabIndex = 2;
             // 
             // textBoxKontainer
@@ -111,12 +112,13 @@
             this.textBoxKontainer.Location = new System.Drawing.Point(8, 23);
             this.textBoxKontainer.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textBoxKontainer.Name = "textBoxKontainer";
-            this.textBoxKontainer.Size = new System.Drawing.Size(537, 22);
+            this.textBoxKontainer.Size = new System.Drawing.Size(465, 20);
             this.textBoxKontainer.TabIndex = 4;
             // 
             // buttonClose
             // 
-            this.buttonClose.Location = new System.Drawing.Point(405, 410);
+            this.buttonClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.buttonClose.Location = new System.Drawing.Point(390, 388);
             this.buttonClose.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(120, 28);
@@ -127,7 +129,8 @@
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(277, 410);
+            this.buttonOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.buttonOk.Location = new System.Drawing.Point(260, 388);
             this.buttonOk.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(120, 28);
@@ -145,7 +148,7 @@
             this.groupBoxPath.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxPath.Name = "groupBoxPath";
             this.groupBoxPath.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxPath.Size = new System.Drawing.Size(555, 98);
+            this.groupBoxPath.Size = new System.Drawing.Size(494, 98);
             this.groupBoxPath.TabIndex = 3;
             this.groupBoxPath.TabStop = false;
             this.groupBoxPath.Text = "Pfad zum Kontainer";
@@ -156,7 +159,7 @@
             this.textBoxSelectedDrive.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textBoxSelectedDrive.Name = "textBoxSelectedDrive";
             this.textBoxSelectedDrive.ReadOnly = true;
-            this.textBoxSelectedDrive.Size = new System.Drawing.Size(429, 22);
+            this.textBoxSelectedDrive.Size = new System.Drawing.Size(356, 20);
             this.textBoxSelectedDrive.TabIndex = 13;
             // 
             // checkBoxAutomountUsb
@@ -165,7 +168,7 @@
             this.checkBoxAutomountUsb.Location = new System.Drawing.Point(8, 65);
             this.checkBoxAutomountUsb.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxAutomountUsb.Name = "checkBoxAutomountUsb";
-            this.checkBoxAutomountUsb.Size = new System.Drawing.Size(149, 21);
+            this.checkBoxAutomountUsb.Size = new System.Drawing.Size(116, 17);
             this.checkBoxAutomountUsb.TabIndex = 6;
             this.checkBoxAutomountUsb.Text = "Automount by USB";
             this.checkBoxAutomountUsb.UseVisualStyleBackColor = true;
@@ -179,14 +182,15 @@
             this.groupBoxKyfilename.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxKyfilename.Name = "groupBoxKyfilename";
             this.groupBoxKyfilename.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxKyfilename.Size = new System.Drawing.Size(273, 86);
+            this.groupBoxKyfilename.Size = new System.Drawing.Size(230, 86);
             this.groupBoxKyfilename.TabIndex = 6;
             this.groupBoxKyfilename.TabStop = false;
             this.groupBoxKyfilename.Text = "Keyfilename";
             // 
             // buttonSelectKeyfile
             // 
-            this.buttonSelectKeyfile.Location = new System.Drawing.Point(165, 55);
+            this.buttonSelectKeyfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.buttonSelectKeyfile.Location = new System.Drawing.Point(117, 50);
             this.buttonSelectKeyfile.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonSelectKeyfile.Name = "buttonSelectKeyfile";
             this.buttonSelectKeyfile.Size = new System.Drawing.Size(101, 28);
@@ -201,7 +205,7 @@
             this.checkBoxNoKeyfile.Location = new System.Drawing.Point(8, 55);
             this.checkBoxNoKeyfile.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxNoKeyfile.Name = "checkBoxNoKeyfile";
-            this.checkBoxNoKeyfile.Size = new System.Drawing.Size(104, 21);
+            this.checkBoxNoKeyfile.Size = new System.Drawing.Size(81, 17);
             this.checkBoxNoKeyfile.TabIndex = 8;
             this.checkBoxNoKeyfile.Text = "Kein Keyfile";
             this.checkBoxNoKeyfile.UseVisualStyleBackColor = true;
@@ -212,7 +216,7 @@
             this.textBoxKeyfile.Location = new System.Drawing.Point(8, 23);
             this.textBoxKeyfile.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.textBoxKeyfile.Name = "textBoxKeyfile";
-            this.textBoxKeyfile.Size = new System.Drawing.Size(255, 22);
+            this.textBoxKeyfile.Size = new System.Drawing.Size(210, 20);
             this.textBoxKeyfile.TabIndex = 7;
             // 
             // groupBoxDriveletter
@@ -222,7 +226,7 @@
             this.groupBoxDriveletter.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxDriveletter.Name = "groupBoxDriveletter";
             this.groupBoxDriveletter.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxDriveletter.Size = new System.Drawing.Size(273, 57);
+            this.groupBoxDriveletter.Size = new System.Drawing.Size(230, 42);
             this.groupBoxDriveletter.TabIndex = 12;
             this.groupBoxDriveletter.TabStop = false;
             this.groupBoxDriveletter.Text = "Zuzuweisender Laufwerksbuchstaben";
@@ -232,10 +236,10 @@
             this.comboBoxDriveletter.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
             this.comboBoxDriveletter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxDriveletter.FormattingEnabled = true;
-            this.comboBoxDriveletter.Location = new System.Drawing.Point(107, 23);
+            this.comboBoxDriveletter.Location = new System.Drawing.Point(73, 17);
             this.comboBoxDriveletter.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.comboBoxDriveletter.Name = "comboBoxDriveletter";
-            this.comboBoxDriveletter.Size = new System.Drawing.Size(59, 23);
+            this.comboBoxDriveletter.Size = new System.Drawing.Size(59, 21);
             this.comboBoxDriveletter.TabIndex = 13;
             this.comboBoxDriveletter.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.ComboBoxDriveletter_DrawItem);
             this.comboBoxDriveletter.MeasureItem += new System.Windows.Forms.MeasureItemEventHandler(this.ComboBoxDriveletter_MeasureItem);
@@ -249,11 +253,11 @@
             this.groupBoxMountoptions.Controls.Add(this.checkBoxRemovable);
             this.groupBoxMountoptions.Controls.Add(this.checkBoxAutomountUsb);
             this.groupBoxMountoptions.Controls.Add(this.checkBoxReadonly);
-            this.groupBoxMountoptions.Location = new System.Drawing.Point(299, 190);
+            this.groupBoxMountoptions.Location = new System.Drawing.Point(256, 190);
             this.groupBoxMountoptions.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxMountoptions.Name = "groupBoxMountoptions";
             this.groupBoxMountoptions.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxMountoptions.Size = new System.Drawing.Size(273, 121);
+            this.groupBoxMountoptions.Size = new System.Drawing.Size(254, 121);
             this.groupBoxMountoptions.TabIndex = 14;
             this.groupBoxMountoptions.TabStop = false;
             this.groupBoxMountoptions.Text = "Mounteinstellungen";
@@ -264,7 +268,7 @@
             this.checkBoxTrueCrypt.Location = new System.Drawing.Point(161, 42);
             this.checkBoxTrueCrypt.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxTrueCrypt.Name = "checkBoxTrueCrypt";
-            this.checkBoxTrueCrypt.Size = new System.Drawing.Size(93, 21);
+            this.checkBoxTrueCrypt.Size = new System.Drawing.Size(72, 17);
             this.checkBoxTrueCrypt.TabIndex = 18;
             this.checkBoxTrueCrypt.Text = "TrueCrypt";
             this.checkBoxTrueCrypt.UseVisualStyleBackColor = true;
@@ -276,7 +280,7 @@
             this.checkBoxPim.Location = new System.Drawing.Point(161, 18);
             this.checkBoxPim.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxPim.Name = "checkBoxPim";
-            this.checkBoxPim.Size = new System.Drawing.Size(53, 21);
+            this.checkBoxPim.Size = new System.Drawing.Size(45, 17);
             this.checkBoxPim.TabIndex = 17;
             this.checkBoxPim.Text = "PIM";
             this.checkBoxPim.UseVisualStyleBackColor = true;
@@ -287,7 +291,7 @@
             this.checkBoxAutomountStart.Location = new System.Drawing.Point(8, 89);
             this.checkBoxAutomountStart.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxAutomountStart.Name = "checkBoxAutomountStart";
-            this.checkBoxAutomountStart.Size = new System.Drawing.Size(202, 21);
+            this.checkBoxAutomountStart.Size = new System.Drawing.Size(152, 17);
             this.checkBoxAutomountStart.TabIndex = 7;
             this.checkBoxAutomountStart.Text = "Automount by programstart";
             this.checkBoxAutomountStart.UseVisualStyleBackColor = true;
@@ -298,7 +302,7 @@
             this.checkBoxRemovable.Location = new System.Drawing.Point(8, 18);
             this.checkBoxRemovable.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxRemovable.Name = "checkBoxRemovable";
-            this.checkBoxRemovable.Size = new System.Drawing.Size(101, 21);
+            this.checkBoxRemovable.Size = new System.Drawing.Size(80, 17);
             this.checkBoxRemovable.TabIndex = 15;
             this.checkBoxRemovable.Text = "Removable";
             this.checkBoxRemovable.UseVisualStyleBackColor = true;
@@ -309,7 +313,7 @@
             this.checkBoxReadonly.Location = new System.Drawing.Point(8, 42);
             this.checkBoxReadonly.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxReadonly.Name = "checkBoxReadonly";
-            this.checkBoxReadonly.Size = new System.Drawing.Size(139, 21);
+            this.checkBoxReadonly.Size = new System.Drawing.Size(108, 17);
             this.checkBoxReadonly.TabIndex = 16;
             this.checkBoxReadonly.Text = "Schreibgeschützt";
             this.checkBoxReadonly.UseVisualStyleBackColor = true;
@@ -318,11 +322,11 @@
             // 
             this.groupBoxHash.Controls.Add(this.comboBoxHash);
             this.groupBoxHash.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.groupBoxHash.Location = new System.Drawing.Point(16, 341);
+            this.groupBoxHash.Location = new System.Drawing.Point(16, 329);
             this.groupBoxHash.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxHash.Name = "groupBoxHash";
             this.groupBoxHash.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxHash.Size = new System.Drawing.Size(273, 55);
+            this.groupBoxHash.Size = new System.Drawing.Size(230, 46);
             this.groupBoxHash.TabIndex = 18;
             this.groupBoxHash.TabStop = false;
             this.groupBoxHash.Text = "Hash";
@@ -330,10 +334,10 @@
             // comboBoxHash
             // 
             this.comboBoxHash.FormattingEnabled = true;
-            this.comboBoxHash.Location = new System.Drawing.Point(73, 18);
+            this.comboBoxHash.Location = new System.Drawing.Point(42, 15);
             this.comboBoxHash.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.comboBoxHash.Name = "comboBoxHash";
-            this.comboBoxHash.Size = new System.Drawing.Size(125, 24);
+            this.comboBoxHash.Size = new System.Drawing.Size(125, 21);
             this.comboBoxHash.TabIndex = 0;
             this.comboBoxHash.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBoxHash_MouseClick);
             // 
@@ -342,11 +346,11 @@
             this.groupBoxSavePassword.Controls.Add(this.checkBoxPassword);
             this.groupBoxSavePassword.Controls.Add(this.buttonShowPassword);
             this.groupBoxSavePassword.Controls.Add(this.buttonSavePassword);
-            this.groupBoxSavePassword.Location = new System.Drawing.Point(299, 318);
+            this.groupBoxSavePassword.Location = new System.Drawing.Point(256, 313);
             this.groupBoxSavePassword.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.groupBoxSavePassword.Name = "groupBoxSavePassword";
             this.groupBoxSavePassword.Padding = new System.Windows.Forms.Padding(5, 4, 5, 4);
-            this.groupBoxSavePassword.Size = new System.Drawing.Size(273, 79);
+            this.groupBoxSavePassword.Size = new System.Drawing.Size(254, 67);
             this.groupBoxSavePassword.TabIndex = 19;
             this.groupBoxSavePassword.TabStop = false;
             this.groupBoxSavePassword.Text = "groupBox1";
@@ -354,10 +358,10 @@
             // checkBoxPassword
             // 
             this.checkBoxPassword.AutoSize = true;
-            this.checkBoxPassword.Location = new System.Drawing.Point(9, 52);
+            this.checkBoxPassword.Location = new System.Drawing.Point(11, 45);
             this.checkBoxPassword.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.checkBoxPassword.Name = "checkBoxPassword";
-            this.checkBoxPassword.Size = new System.Drawing.Size(98, 21);
+            this.checkBoxPassword.Size = new System.Drawing.Size(80, 17);
             this.checkBoxPassword.TabIndex = 4;
             this.checkBoxPassword.Text = "checkBox1";
             this.checkBoxPassword.UseVisualStyleBackColor = true;
@@ -365,7 +369,8 @@
             // 
             // buttonShowPassword
             // 
-            this.buttonShowPassword.Location = new System.Drawing.Point(139, 18);
+            this.buttonShowPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.buttonShowPassword.Location = new System.Drawing.Point(120, 15);
             this.buttonShowPassword.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonShowPassword.Name = "buttonShowPassword";
             this.buttonShowPassword.Size = new System.Drawing.Size(101, 28);
@@ -376,7 +381,8 @@
             // 
             // buttonSavePassword
             // 
-            this.buttonSavePassword.Location = new System.Drawing.Point(9, 18);
+            this.buttonSavePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.buttonSavePassword.Location = new System.Drawing.Point(9, 15);
             this.buttonSavePassword.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.buttonSavePassword.Name = "buttonSavePassword";
             this.buttonSavePassword.Size = new System.Drawing.Size(101, 28);
@@ -392,9 +398,9 @@
             // 
             // NewContainer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 453);
+            this.ClientSize = new System.Drawing.Size(519, 423);
             this.Controls.Add(this.groupBoxSavePassword);
             this.Controls.Add(this.groupBoxHash);
             this.Controls.Add(this.groupBoxMountoptions);
@@ -405,7 +411,7 @@
             this.Controls.Add(this.groupBoxPath);
             this.Controls.Add(this.groupBoxDescription);
             this.DoubleBuffered = true;
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
